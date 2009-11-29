745153df91dbe1ab533ad76b1a68f158de249a96
baicaibang <baicaibang@70501136-4834-11de-8855-c187e5f49513>
2009-11-29 11:23:44 +0000

    20091129 OpenCV

---
+++ trunk/Learning OpenCV/Chapter 4 - HighGUI/Exercise_4_8.cpp
@@ -0,0 +1,301 @@
+/////////////////////// COPY & MODIFY ! ///////////////////////
+///////////////////////////////////////////////////////////////
+// Date:       2009-10-18
+// Modify:     2009-10-19
+// Coder:      Yishi Guo
+// Copy From:  OpenCV\samples\c\facedetect.c
+// Exercise:   4.8
+// Page:       108p
+// Chapter:    4 HighGUI
+// Content:    Face fun
+///////////////////////////////////////////////////////////////
+#include "cv.h"
+#include "highgui.h"
+
+#include <iostream.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+
+#ifdef _EiC
+#define WIN32
+#endif
+
+IplImage *image = 0;
+
+// Skull image
+IplImage *skull_image = 0;
+
+static CvMemStorage* storage = 0;
+static CvHaarClassifierCascade* cascade = 0;
+static CvHaarClassifierCascade* nested_cascade = 0;
+int use_nested_cascade = 0;
+int g_pos = 0;
+
+void detect_and_draw( IplImage* image );
+void alpha_callback( int pos );
+
+const char* cascade_name = 
+	"../../Test Resource/data/haarcascades/haarcascade_frontalface_alt.xml";
+const char* nested_cascade_name = 
+	"../../Test Resource/data/haarcascades/haarcascade_eye.xml";
+const char* skull_image_name = "";
+double scale = 1;
+
+int main( int argc, char** argv ) {
+	CvCapture* capture = 0;
+	IplImage *frame, *frame_copy = 0;
+
+	const char* scale_opt = "--scale=";
+	int scale_opt_len = (int)strlen(scale_opt);
+	const char* cascade_opt = "--cascade=";
+	int cascade_opt_len = (int)strlen(cascade_opt);
+	const char* nested_cascade_opt = "--nested-cascaed";
+	int nested_cascade_opt_len = (int)strlen(nested_cascade_opt);
+
+	// Skull image - (2009-10-19)
+	// 
+	const char* skull_image_opt = "--skull-image=";
+	int skull_image_opt_len = (int)strlen(skull_image_opt);
+
+	int i;
+	const char* input_name = 0;
+
+	for( i = 0; i < argc; i++ ) {
+		if ( strncmp( argv[i], cascade_opt, cascade_opt_len) == 0 ) {
+			cascade_name = argv[i] + cascade_opt_len;
+		} else if ( strncmp( argv[i], nested_cascade_opt, nested_cascade_opt_len ) == 0 ) {
+			if ( argv[i][nested_cascade_opt_len] == '=' ) {
+				nested_cascade_name = argv[i] + nested_cascade_opt_len + 1;
+			}
+			nested_cascade = (CvHaarClassifierCascade*)cvLoad( nested_cascade_name, 0, 0, 0 );
+			if ( !nested_cascade )
+				fprintf( stderr, "WARNING: Could not load classifier cascade for nested objects\n" );
+		} else if ( strncmp( argv[i], scale_opt, scale_opt_len ) == 0 ) {
+			if ( !sscanf( argv[i] + scale_opt_len, "%lf", &scale ) || scale < 1 )
+				scale = 1;
+		} else if ( strncmp( argv[i], skull_image_opt, skull_image_opt_len ) == 0 ) {
+			skull_image_name = argv[i] + skull_image_opt_len;
+			skull_image = cvLoadImage( skull_image_name );
+			printf( "%s\n", skull_image_name );
+		} else if ( argv[i][0] == '-' ) {
+			fprintf( stderr, "WARNING: Unknown option %s\n", argv[i] );
+		} else {
+			input_name = argv[i];
+		}
+	}
+
+	cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
+
+	if ( !cascade ) {
+		fprintf( stderr, "ERROR: Could not load classifier cascade\n" );
+		fprintf( stderr,
+			"Usage: facedetect [--cascade=\"<cascade_path>\"]\n"
+			"   [--nested-cascade[=\"nested_cascade_path\"]]\n"
+			"   [--scale[=<image scale>\n"
+			"   [--skull-image=<skull image>\n"
+			"   [filename|camera_index]\n" );
+		return -1;
+	}
+	storage = cvCreateMemStorage(0);
+
+	if ( !input_name || (isdigit(input_name[0]) && input_name[1] == '\0') )
+		capture = cvCaptureFromCAM( !input_name ? 0 : input_name[0] - '0' );
+	else if ( input_name ) {
+		image = cvLoadImage( input_name, 1 );
+		if ( !image ) {
+			capture = cvCaptureFromAVI( input_name );
+		}
+	}else {
+		image = cvLoadImage( "ChenLi.jpg", 1 );
+	}
+
+	cvNamedWindow( "result", 1 );
+	cvCreateTrackbar(
+		"Alpha",
+		"result",
+		&g_pos,
+		10,
+		alpha_callback
+	);
+
+	if ( capture ) {
+		for (;;) {
+			if ( !cvGrabFrame( capture ) )
+				break;
+			frame = cvRetrieveFrame( capture );
+			if ( !frame )
+				break;
+			if ( !frame_copy )
+				frame_copy = cvCreateImage( cvSize(frame->width, frame->height),
+				                            IPL_DEPTH_8U, frame->nChannels );
+			if ( frame->origin == IPL_ORIGIN_TL )
+				cvCopy( frame, frame_copy, 0 );
+			else
+				cvFlip( frame, frame_copy, 0 );
+			detect_and_draw( frame_copy );
+
+			if ( cvWaitKey( 33 ) >= 0 ) {
+				goto _cleanup_;
+			}
+		}
+
+		cvWaitKey(0);
+
+_cleanup_:
+		cvReleaseImage( &frame_copy );
+		cvReleaseCapture( &capture );
+
+	} else {
+		if ( image ) {
+			detect_and_draw( image );
+			cvWaitKey(0);
+			cvReleaseImage( &image );
+		} else if ( input_name ) {
+			// assume it is a text file containing the
+			// list of the image filenames to be processed - one per line
+			FILE* f = fopen( input_name, "rt" );
+			if ( f ) {
+				char buf[1000+1];
+				while ( fgets( buf, 1000, f ) ) {
+					int len = (int)strlen(buf), c;
+					while ( len > 0 && isspace( buf[len-1]) )
+						len--;
+					buf[len] = '\0';
+					printf( "file %s\n", buf );
+					image = cvLoadImage( buf, 1 );
+					if ( image ) {
+						detect_and_draw( image );
+						c = cvWaitKey(0);
+						if ( c == 27 || c == 'q' || c == 'Q' )
+							break;
+						cvReleaseImage( &image );
+					}
+				}
+				fclose(f);
+			}
+		}
+	}
+	cvDestroyWindow( "result" );
+	
+	return 0;
+}
+
+void detect_and_draw( IplImage* src ) {
+	IplImage* img = cvCloneImage( src );
+	static CvScalar colors[] = {
+		{{0, 0, 255}},
+		{{0, 128, 255}},
+		{{0, 255, 255}},
+		{{0, 255, 0}},
+		{{255, 128, 0}},
+		{{255, 255, 0}},
+		{{255, 0, 0}},
+		{{255, 0, 255}}
+	};
+
+	IplImage *gray, *small_img;
+	int i, j;
+
+	gray = cvCreateImage( cvSize(img->width, img->height), 8, 1 );
+	small_img = cvCreateImage( cvSize( cvRound (img->width/scale),
+		                                 cvRound( img->height/scale)), 8, 1 );
+	cvCvtColor( img, gray, CV_BGR2GRAY );
+	cvResize( gray, small_img, CV_INTER_LINEAR );
+	cvEqualizeHist( small_img, small_img );
+	cvClearMemStorage( storage );
+
+	if ( cascade ) {
+		double t = (double)cvGetTickCount();
+		CvSeq* faces = cvHaarDetectObjects( small_img, cascade, storage,
+			                                1.1, 2, 0
+			                                // |CV_HAAR_FIND_BIGGEST_OBJECT
+											// | CV_HAAR_DO_ROUGH_SEARCH
+											| CV_HAAR_DO_CANNY_PRUNING
+											// | CV_HAAR_SCALE_IMAGE
+											,
+											cvSize( 30, 30 ) );
+		t = (double)cvGetTickCount() - t;
+		printf( "detect time = %gms\n", t/((double)cvGetTickFrequency()*1000.) );
+		for( i = 0; i < (faces ? faces->total : 0); i++ ) {
+			CvRect* r = (CvRect*)cvGetSeqElem( faces, i );
+			CvMat small_img_roi;
+			CvSeq* nested_objects;
+			CvPoint center;
+			CvScalar color = colors[i%8];
+			int radius;
+			
+			// center.x = cvRound((r->x + r->width*0.5)*scale);
+			// center.y = cvRound((r->y + r->height*0.5)*scale);
+			// radius = cvRound((r->width + r->height)*0.25*scale);
+			// cvCircle( img, center, radius, color, 3, 8, 0 );
+
+			// 2009-10-19
+			// Draw a rectangle
+			//
+			CvPoint pt1 = cvPoint( r->x*scale , r->y*scale );
+			CvPoint pt2 = cvPoint( (r->x+r->width)*scale, (r->y+r->height)*scale );
+			double alpha = g_pos / 1;
+			double beta = 1.0;
+
+			cvRectangle( img, pt1, pt2, color, 3, 8, 0 );
+
+			if ( skull_image ) {
+				IplImage* dst = cvCreateImage( cvSize(r->width*scale, r->height*scale), img->depth, img->nChannels );
+				// For debug
+				//
+				cvResize( skull_image, dst );
+
+				CvRect rect = cvRect( r->x*scale, r->y*scale, r->width*scale, r->height*scale );
+				// For debug
+				// 
+				cout << "Rect: " << rect.x << " " << rect.y << " " << rect.width << " " << rect.height << endl;
+				cout << "Size: " << dst->width << " " << dst->height << endl;
+
+				cvSetImageROI( img, rect );
+				// cvCopy( dst, img );
+				cvAddWeighted( dst, alpha, img, beta, 0.0, img );
+				cvResetImageROI( img );
+
+				cvReleaseImage( &dst );
+			}
+
+			if ( !nested_cascade )
+				continue;
+			cvGetSubRect( small_img, &small_img_roi, *r );
+			nested_objects = cvHaarDetectObjects( &small_img_roi, nested_cascade, storage,
+				1.1, 2, 0
+				// | CV_HAAR_FIND_BIGGEST_OBJECT
+				// | CV_HAAR_DO_ROUGH_SERRCH
+				// | CV_HAAR_DO_CANNY_PRUNING
+				// | CV_HAAR_SCALE_IMAGE
+				,
+				cvSize(0, 0) );
+			for ( j = 0; j < (nested_objects ? nested_objects->total : 0); j++ ) {
+				CvRect* nr = (CvRect*)cvGetSeqElem( nested_objects, j );
+				center.x = cvRound((r->x + nr->x + nr->width*0.5)*scale);
+				center.y = cvRound((r->y + nr->y + nr->height*0.5)*scale);
+				radius = cvRound((nr->width + nr->height)*0.25*scale);
+				cvCircle( img, center, radius, color, 3, 8, 0 );
+			}
+		}
+	}
+	
+	cvShowImage( "result", img );
+	cvReleaseImage( &gray );
+	cvReleaseImage( &small_img );
+}
+
+void alpha_callback( int pos ) {
+	IplImage* temp = cvCloneImage( image );
+
+	detect_and_draw( temp );
+
+	cvReleaseImage( &temp );
+}
